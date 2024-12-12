bd85bf313c125499bfb8581f933bbc38c910d7fd
MM\nilufar <nilufar@mail.com>
2024-12-12 10:41:33 +0500

    Add Models

---
+++ MacMVC/Models/Role.cs
@@ -0,0 +1,22 @@
+﻿namespace MacMVC.Models
+{
+    public class Role
+    {
+        /// <summary>
+        /// Идетификатор
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Описание
+        /// </summary>
+        public string? Description { get; set; }
+        /// <summary>
+        /// Пользователи с этой ролью 
+        /// </summary>
+        public ICollection<User> Users { get; set; }
+    }
+}
---
+++ MacMVC/Models/User.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MacMVC.Models
+{
+    public class User
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Фамилия 
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Отчество 
+        /// </summary>
+        public string? MiddleName { get; set; }
+        /// <summary>
+        /// Электронная почта
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// Номер телефона
+        /// </summary>
+        public string Phone { get; set; }
+        /// <summary>
+        /// Связь пользователя с его ролью
+        /// </summary>
+        [ForeignKey("RoleId")]
+        public int RoleId { get; set; }
+        /// <summary>
+        /// Роль
+        /// </summary>
+        public Role Role { get; set; }
+        /// <summary>
+        /// Список созданных заказов пользователя
+        /// </summary>
+        public ICollection<Order> CreatedOrders { get; set; }
+        /// <summary>
+        /// Список подтвержденных заказов пользователя
+        /// </summary>
+        public ICollection<Order> ConfirmedOrders { get; set; }
+    }
+}
